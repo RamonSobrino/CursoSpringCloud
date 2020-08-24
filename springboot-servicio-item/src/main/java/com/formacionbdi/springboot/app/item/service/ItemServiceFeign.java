1222907fea80747a0f4ab74818eb97510fd8108f
Ramon Sobrino <ramon.sobrino@grupoiris.com>
2020-08-25 00:28:07 +0200

    first commit

---
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/service/ItemServiceFeign.java
@@ -0,0 +1,27 @@
+package com.formacionbdi.springboot.app.item.service;
+
+import com.formacionbdi.springboot.app.item.clientes.ProductoClienteRest;
+import com.formacionbdi.springboot.app.item.models.Item;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service("serviceFeign")
+public class ItemServiceFeign implements ItemService{
+
+    @Autowired
+    private ProductoClienteRest productoFeign;
+
+    @Override
+    public List<Item> findAll() {
+        return productoFeign.listar().stream().map(producto -> new Item(producto, 1)).collect(Collectors.toList());
+    }
+
+    @Override
+    public Item findById(Long id, Integer cantidad) {
+        return new Item(productoFeign.detalle(id), cantidad);
+    }
+}
---
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
@@ -0,0 +1,45 @@
+package com.formacionbdi.springboot.app.productos.controllers;
+
+import com.formacionbdi.springboot.app.productos.models.entity.Producto;
+import com.formacionbdi.springboot.app.productos.models.service.IProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class ProductoController {
+
+    @Autowired
+    private Environment environment;
+
+    @Value("${server.port}")
+    private Integer port;
+
+    @Autowired
+    private IProductoService productoService;
+
+    @GetMapping("/listar")
+    public List<Producto> getProductos(){
+        return productoService.findAll().stream()
+                .map(producto -> {
+                    producto.setPort(port);
+                    return producto;
+                }).collect(Collectors.toList());
+    }
+
+
+    @GetMapping("/ver/{id}")
+    public Producto detalle(@PathVariable Long id){
+        Producto producto = productoService.findById(id);
+        producto.setPort(port);
+        return productoService.findById(id);
+    }
+
+}
